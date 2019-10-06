710c4d497de3f043138f6dae7dc59f21f06b5b0c
paulyo19 <catkillercleary@gmail.com>
2019-10-06 12:32:36 +0100

    adding notes to the javascript file so i can work out how to do this project

---
+++ script.js
@@ -0,0 +1,25 @@
+// PLANNING STEP 1
+// A). ADD EVENT HANDLER.... FOR THE THICK SIGN BESIDE THE VALUE AND DECRIPTION PLACEHOLDER
+// B). GET INPUT VALUES.... DATA FOR THE DESCRIPTION PLACEHOLDER
+// C). THEN WHEN WE HAVE THE DATA WE NEED TO ADD IT TO OUR DATA STRUCTURE LOCATED UNERNEATH
+// D). THEN ADD NE ITEM TO THE USER INTERFACE
+// E). THEN THE BUDGET INTERFACE AT THE TOP IS UPDATED SO WE MUST GET THE RIGHT CALCULATIONS FOR THAT
+// F). THEN AGAIN WE MUST UPDATE THE UI INTERFACE .... THE DESCRIPTON PLACHOLDER
+
+
+//===============================================================================================================
+// STRUCTURE OUR CODE
+//===============================================================================================================
+//       UI MODULE             |           DATA MODULE                         |       CONTROLLER MODULE        |
+// B). GET INPUT DATA          |    C). ADD NEW ITEM TO OUR DATA STRUCTURE     |  A). ADD EVENT HANDLER         |
+// D). ADD NEW ITEM TO THE UI  |    E). CALCULATE THE BUDGET                   |                                |
+// F). UPDATE THE UI           |                                               |                                |
+//================================================================================================================
+
+//==========================================
+// IMPLEMTING THE MODULE
+//==========================================
+// WHAT WE LEARN
+// A). HOW TO USE THE MODULE PATTERN
+// B). MORE ABOUT PRIVATE AND PUBLIC DATA, ENCAPSULATION AND SEPERATION OF CONCERNS
+//===========================================
